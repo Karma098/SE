a864aef3d61d5107b10f198eb6306306a2366512
sumi152 <sumitrohit2004@gmail.com>
2023-03-18 16:41:11 +0530

    register

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
 <head>
   <title>Login</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  
 </head>
 
 <body>
   <div class="back">
     <div class="container">
       <h1>Login</h1>
-      <form>
+      <form id="f1">
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+        <input type="text" id="username" name="username" required >
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
         <button type="submit">Login</button>
       </form><br>
-      <p>New user? <a href="#">Create an account</a></p>
+      <p>New user? <a href="register.html">Create an account</a></p>
     </div>
   </div>
 </body>
